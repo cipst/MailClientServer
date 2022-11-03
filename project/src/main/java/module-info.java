e81017d86b5a08ec9851b213245677a7a6f09bdd
st203183 <cipolletta.stefano@educ.di.unito.it>
2022-11-03 14:32:43 +0100

    Init project

---
+++ project/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.project.project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.project.project to javafx.fxml;
+    exports com.project.project;
+}
