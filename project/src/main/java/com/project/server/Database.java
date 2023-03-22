b18c8d92a958f59fbe428e5beff8979b249367e7
st203183 <cipolletta.stefano@educ.di.unito.it>
2023-03-22 11:50:18 +0100

    Database on load creation and population file

--- project/src/main/java/com/project/server/Database.java
+++ project/src/main/java/com/project/server/Database.java
@@ -1,21 +1,60 @@
 package com.project.server;
 
+import com.project.server.model.ClientModel;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
 
-    private static final String DATABASE_PATH = Database.class.getResource("").getPath();
+    private final String DATABASE_PATH = "src/main/resources/com/project/server/database.txt";
+    private ArrayList<ClientModel> clients;
 
     public Database(){
-//        try{
-//
-//            File file = new File(Database.class.getResource("log.txt").getFile());
-//
-//        }catch(IOException e){
-//            System.out.println("Error: " + e.getMessage());
-//        }
-
+        clients = new ArrayList<>();
+        onLoad();
     }
 
+    private void onLoad(){
+        try{
+            File file = new File(DATABASE_PATH);
+            if (file.createNewFile())
+                System.out.println("File created: " + file.getName());
+            else
+                System.out.println("File already exists.");
+
+            FileWriter writer = new FileWriter(DATABASE_PATH, false);
+
+            String json = loadUsers();
+
+            writer.write(json);
+            writer.close();
+        }catch(Exception e){
+            System.out.println("ERROR: " + e);
+        }
+    }
+
+    private String loadUsers(){
+        StringBuilder json = new StringBuilder("[\n");
+        try{
+            HashMap<String, String> accounts = new HashMap<>(3);
+
+            accounts.put("stefano.cipolletta@unito.it", "stefano.cipolletta");
+            accounts.put("matteo.barone@unito.it", "matteo.barone");
+            accounts.put("alessio.rosa@unito.it", "alessio.rosa");
+
+            for(String email : accounts.keySet()){
+                String password = accounts.get(email);
+                json.append(String.format("\t{\n\t\t\"email\":\"%s\",\n\t\t\"password\":\"%s\"\n\t},\n", email, password));
+            }
+            json.append("]");
+
+        }catch(Exception e){
+            System.out.println("ERROR: " + e);
+        }
+
+        return json.toString();
+    }
 
 }
