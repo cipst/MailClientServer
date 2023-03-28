41e974c5fbcac908b6f4ce6023f0dff7a6cf0d14
st203183 <cipolletta.stefano@educ.di.unito.it>
2023-03-28 18:00:29 +0200

    Email + EmailConverter + EmailSerializable

--- project/src/main/java/com/project/models/Email.java
+++ project/src/main/java/com/project/models/Email.java
@@ -4,55 +4,109 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Model class for an email
  */
-public class Email implements java.io.Serializable {
+public class Email {
 
-    private final String sender;
-    private final ArrayList<String> recipients;
-    private final String subject;
-    private final String message;
+    private int id;
+    private StringProperty sender;
+    private ObservableList<String> recipients;
+    private StringProperty subject;
+    private StringProperty message;
+    private StringProperty date;
 
-    private final Date date;
-
-    public Email(String sender, ArrayList<String> recipients, String subject, String message, Date date) {
-        this.sender = sender;
-        this.recipients = recipients;
-        this.subject = subject;
-        this.message = message;
-        this.date = date;
+    public Email(int id, String sender, ArrayList<String> recipients, String subject, String message, String date) {
+        this.id = id;
+        this.sender = new SimpleStringProperty(sender);
+        this.recipients = FXCollections.observableArrayList(recipients);
+        this.subject = new SimpleStringProperty(subject);
+        this.message = new SimpleStringProperty(message);
+        this.date = new SimpleStringProperty(date);
     }
 
+    public Email(String sender, ArrayList<String> recipients, String subject, String message, String date) {
+        this.id = -1;
+        this.sender = new SimpleStringProperty(sender);
+        this.recipients = FXCollections.observableArrayList(recipients);
+        this.subject = new SimpleStringProperty(subject);
+        this.message = new SimpleStringProperty(message);
+        this.date = new SimpleStringProperty(date);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getSender() {
+        return sender.get();
+    }
+
+    public StringProperty senderProperty() {
         return sender;
     }
 
-    public ArrayList<String> getRecipients() {
+    public void setSender(String sender) {
+        this.sender.set(sender);
+    }
+
+    public List<String> getRecipients() {
         return recipients;
     }
 
+    public void setRecipients(List<String> recipients) {
+        this.recipients.setAll(recipients);
+    }
+
     public String getSubject() {
+        return subject.get();
+    }
+
+    public StringProperty subjectProperty() {
         return subject;
     }
 
+    public void setSubject(String subject) {
+        this.subject.set(subject);
+    }
+
     public String getMessage() {
+        return message.get();
+    }
+
+    public StringProperty messageProperty() {
         return message;
     }
 
-    public String getDate() {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+    public void setMessage(String message) {
+        this.message.set(message);
     }
 
-    @Override
-    public String toString() {
-        return String.format("Sender: %s - Recipients: %s - Subject: %s - Message: %s - Date: %s", sender, recipients, subject, message, date);
+    public String getDate() {
+        return date.get();
+    }
+
+    public StringProperty dateProperty() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date.set(date);
     }
 }
---
+++ project/src/main/java/com/project/models/EmailSerializable.java
@@ -0,0 +1,58 @@
+package com.project.models;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class EmailSerializable implements java.io.Serializable {
+
+    private int id;
+    private String sender;
+    private ArrayList<String> recipients;
+    private String subject;
+    private String message;
+
+    private String date;
+
+    public EmailSerializable(int id, String sender, ArrayList<String> recipients, String subject, String message, String date) {
+        this.id = id;
+        this.sender = sender;
+        this.recipients = recipients;
+        this.subject = subject;
+        this.message = message;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public ArrayList<String> getRecipients() {
+        return recipients;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Sender: %s - Recipients: %s - Subject: %s - Message: %s - Date: %s", sender, recipients, subject, message, date);
+    }
+}
