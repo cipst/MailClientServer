ad5b39634b170176f88706d3e1b6fcfd99823474
st203183 <cipolletta.stefano@educ.di.unito.it>
2023-03-29 16:08:52 +0200

    ClientGUI

--- project/src/main/java/com/project/client/controller/ClientGUIController.java
+++ project/src/main/java/com/project/client/controller/ClientGUIController.java
@@ -2,6 +2,7 @@ package com.project.client.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.web.WebView;
 
@@ -17,11 +18,49 @@ public class ClientGUIController {
     @FXML
     public Button btnNewEmail;
     @FXML
-    public TreeView<String> treeViewEmails;
+    public TreeView<String> treeViewEmailsInbox;
+    @FXML
+    public TreeView<String> treeViewEmailsOutbox;
     @FXML
     public WebView webViewEmail;
 
     public void initialize() {
         System.out.println("ClientGUIController initialized");
+
+        //Select all SplitPane dividers and set their position to 0.5
+
+
+        webViewEmail.getEngine().loadContent("<h1>Test</h1><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><h1>CIAO</h1>");
+
+        treeViewEmailsInbox.setRoot(new TreeItem<>("Inbox"));
+        treeViewEmailsOutbox.setRoot(new TreeItem<>("Outbox"));
+
+//        treeViewEmailsInbox.getRoot().getChildren().add(new TreeItem<>("Email 1"));
+
+    }
+
+    @FXML
+    public void newEmail(){
+        System.out.println("btnNewEmail clicked");
+    }
+
+    @FXML
+    public void reply() {
+        System.out.println("btnReply clicked");
+    }
+
+    @FXML
+    public void replyAll() {
+        System.out.println("btnReplyAll clicked");
+    }
+
+    @FXML
+    public void forward() {
+        System.out.println("btnForward clicked");
+    }
+
+    @FXML
+    public void delete() {
+        System.out.println("btnDelete clicked");
     }
 }
